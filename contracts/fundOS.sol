1f4b7552432291bcad6ee8df1d9ca52404ad0891
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-02-05 10:22:09 -0700

    begin fundOS contract

---
+++ contracts/fundOS.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: UNLICENSED
+
+// Solidity files have to start with this pragma.
+// It will be used by the Solidity compiler to validate its version.
+pragma solidity ^0.8.9;
+
+// We import this library to be able to use console.log
+import "hardhat/console.sol";
